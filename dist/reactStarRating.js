a971a8f2201e25af406e7cf0baec2fce57d87460
Ashwini Ashish <ashwini.ashish@ril.com>
2019-12-05 00:13:36 +0530

    Removed node-sass

--- dist/reactStarRating.js
+++ dist/reactStarRating.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "./ReactStarRating.scss";
+import "./reactStarRating.css";
 
 class ReactStarRating extends Component {
   constructor(props) {
